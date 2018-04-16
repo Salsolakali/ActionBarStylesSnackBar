ee4fb6206fcf0b74acac1c4d7e6372b47b5e735c
EU\FernanA6 <alberto.fernandezseral@unisys.com>
2018-04-16 17:22:53 +0200

    Con snackbar

--- app/src/main/java/com/example/fernana6/actionbarstylessnackbar/MainActivity.java
+++ app/src/main/java/com/example/fernana6/actionbarstylessnackbar/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.fernana6.actionbarstylessnackbar;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSnackBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSnackBar = findViewById(R.id.btnSnackBar);
+        btnSnackBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, R.string.texto_snack_bar, Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
